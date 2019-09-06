2c713ee394622a875f1896be834bafcc87b3cb29
yumaloop <yumaloop@gmail.com>
2019-09-06 14:45:41 +0900

    update repo 2019年 9月 6日 金曜日 14時45分41秒 JST

--- carracing/extract.py
+++ carracing/extract.py
@@ -6,7 +6,6 @@ import numpy as np
 import random
 import os
 import gym
-
 from model import make_model
 
 MAX_FRAMES = 1000 # max length of carracing
@@ -19,50 +18,47 @@ if not os.path.exists(DIR_NAME):
     os.makedirs(DIR_NAME)
 
 model = make_model(load_model=False)
-
 total_frames = 0
 model.make_env(render_mode=render_mode, full_episode=True)
 for trial in range(MAX_TRIALS): # 200 trials per worker
-  try:
-    random_generated_int = random.randint(0, 2**31-1)
-    filename = DIR_NAME+"/"+str(random_generated_int)+".npz"
-    recording_obs = []
-    recording_action = []
+    try:
+        random_generated_int = random.randint(0, 2**31-1)
+        filename = DIR_NAME+"/"+str(random_generated_int)+".npz"
+        recording_obs = []
+        recording_action = []
 
-    np.random.seed(random_generated_int)
-    model.env.seed(random_generated_int)
+        np.random.seed(random_generated_int)
+        model.env.seed(random_generated_int)
 
-    # random policy
-    model.init_random_model_params(stdev=np.random.rand()*0.01)
+        # random policy
+        model.init_random_model_params(stdev=np.random.rand()*0.01)
+        model.reset()
+        obs = model.env.reset() # pixels
 
-    model.reset()
-    obs = model.env.reset() # pixels
+        for frame in range(MAX_FRAMES):
+            if render_mode:
+                model.env.render("human")
+            else:
+                model.env.render("rgb_array")
 
-    for frame in range(MAX_FRAMES):
-      if render_mode:
-        model.env.render("human")
-      else:
-        model.env.render("rgb_array")
+            recording_obs.append(obs)
+            z, mu, logvar = model.encode_obs(obs)
+            action = model.get_action(z)
+            recording_action.append(action)
+            obs, reward, done, info = model.env.step(action)
 
-      recording_obs.append(obs)
+            if done:
+                break
 
-      z, mu, logvar = model.encode_obs(obs)
-      action = model.get_action(z)
-
-      recording_action.append(action)
-      obs, reward, done, info = model.env.step(action)
-
-      if done:
-        break
-
-    total_frames += (frame+1)
-    print("dead at", frame+1, "total recorded frames for this worker", total_frames)
-    recording_obs = np.array(recording_obs, dtype=np.uint8)
-    recording_action = np.array(recording_action, dtype=np.float16)
-    np.savez_compressed(filename, obs=recording_obs, action=recording_action)
-  except gym.error.Error:
-    print("stupid gym error, life goes on")
-    model.env.close()
-    model.make_env(render_mode=render_mode)
-    continue
+        total_frames += (frame+1)
+        print("dead at", frame+1, "total recorded frames for this worker", total_frames)
+        recording_obs = np.array(recording_obs, dtype=np.uint8)
+        recording_action = np.array(recording_action, dtype=np.float16)
+        np.savez_compressed(filename, obs=recording_obs, action=recording_action)
+    except gym.error.Error:
+        print("stupid gym error, life goes on")
+        model.env.close()
+        model.make_env(render_mode=render_mode)
+        continue
+        
 model.env.close()
--- carracing/series.py
+++ carracing/series.py
@@ -20,31 +20,31 @@ if not os.path.exists(SERIES_DIR):
     os.makedirs(SERIES_DIR)
 
 def load_raw_data_list(filelist):
-  data_list = []
-  action_list = []
-  counter = 0
-  for i in range(len(filelist)):
-    filename = filelist[i]
-    raw_data = np.load(os.path.join(DATA_DIR, filename))
-    data_list.append(raw_data['obs'])
-    action_list.append(raw_data['action'])
-    if ((i+1) % 1000 == 0):
-      print("loading file", (i+1))
-  return data_list, action_list
+    data_list = []
+    action_list = []
+    counter = 0
+    for i in range(len(filelist)):
+        filename = filelist[i]
+        raw_data = np.load(os.path.join(DATA_DIR, filename))
+        data_list.append(raw_data['obs'])
+        action_list.append(raw_data['action'])
+        if ((i+1) % 1000 == 0):
+            print("loading file", (i+1))
+    return data_list, action_list
 
 def encode_batch(batch_img):
-  simple_obs = np.copy(batch_img).astype(np.float)/255.0
-  simple_obs = simple_obs.reshape(batch_size, 64, 64, 3)
-  mu, logvar = vae.encode_mu_logvar(simple_obs)
-  z = (mu + np.exp(logvar/2.0) * np.random.randn(*logvar.shape))
-  return mu, logvar, z
+    simple_obs = np.copy(batch_img).astype(np.float)/255.0
+    simple_obs = simple_obs.reshape(batch_size, 64, 64, 3)
+    mu, logvar = vae.encode_mu_logvar(simple_obs)
+    z = (mu + np.exp(logvar/2.0) * np.random.randn(*logvar.shape))
+    return mu, logvar, z
 
 def decode_batch(batch_z):
-  # decode the latent vector
-  batch_img = vae.decode(z.reshape(batch_size, z_size)) * 255.
-  batch_img = np.round(batch_img).astype(np.uint8)
-  batch_img = batch_img.reshape(batch_size, 64, 64, 3)
-  return batch_img
+    # decode the latent vector
+    batch_img = vae.decode(z.reshape(batch_size, z_size)) * 255.
+    batch_img = np.round(batch_img).astype(np.uint8)
+    batch_img = batch_img.reshape(batch_size, 64, 64, 3)
+    return batch_img
 
 
 # Hyperparameters for ConvVAE
@@ -58,7 +58,6 @@ filelist.sort()
 filelist = filelist[0:10000]
 
 dataset, action_dataset = load_raw_data_list(filelist)
-
 reset_graph()
 
 vae = ConvVAE(z_size=z_size,
@@ -74,15 +73,14 @@ vae.load_json(os.path.join(model_path_name, 'vae.json'))
 mu_dataset = []
 logvar_dataset = []
 for i in range(len(dataset)):
-  data_batch = dataset[i]
-  mu, logvar, z = encode_batch(data_batch)
-  mu_dataset.append(mu.astype(np.float16))
-  logvar_dataset.append(logvar.astype(np.float16))
-  if ((i+1) % 100 == 0):
-    print(i+1)
+    data_batch = dataset[i]
+    mu, logvar, z = encode_batch(data_batch)
+    mu_dataset.append(mu.astype(np.float16))
+    logvar_dataset.append(logvar.astype(np.float16))
+    if ((i+1) % 100 == 0):
+        print(i+1)
 
 action_dataset = np.array(action_dataset)
 mu_dataset = np.array(mu_dataset)
 logvar_dataset = np.array(logvar_dataset)
-
 np.savez_compressed(os.path.join(SERIES_DIR, "series.npz"), action=action_dataset, mu=mu_dataset, logvar=logvar_dataset)
